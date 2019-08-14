3c406f3915270945ce97d24b700871417e0c8328
Sirikon <hello@sirikon.me>
2019-08-15 01:25:17 +0200

    wip

--- run.sh
+++ run.sh
@@ -15,7 +15,66 @@ sleep 3
 #     sleep 0.1
 # done < "$input"
 
-for i in {1..100}
+xdotool type --delay 50 "/clear"
+sleep 1
+xdotool key Return
+sleep 1
+xdotool type --delay 50 "/give Sirikon minecraft:red_wool"
+sleep 1
+xdotool key Return
+sleep 1
+xdotool type --delay 50 "/give Sirikon minecraft:yellow_wool"
+sleep 1
+xdotool key Return
+sleep 1
+
+xdotool type --delay 50 "/setblock 1930 150 10010 minecraft:white_wool"
+sleep 1
+xdotool key Return
+sleep 1
+xdotool type --delay 50 "/setblock 1930 150 10011 minecraft:white_wool"
+sleep 1
+xdotool key Return
+sleep 1
+xdotool type --delay 50 "/setblock 1930 150 10012 minecraft:white_wool"
+sleep 1
+xdotool key Return
+sleep 1
+
+xdotool type --delay 50 "/tp Sirikon 1930 151 10010 0 -90"
+sleep 1
+xdotool key Return
+sleep 1
+
+for i in {1..5}
+do
+    xdotool key 1
+    xdotool keydown space
+    sleep 0.35
+    xdotool click 3
+    xdotool keyup space
+done
+
+xdotool type --delay 50 "/tp Sirikon 1930 151 10011 0 -90"
+sleep 1
+xdotool key Return
+sleep 1
+
+for i in {1..5}
+do
+    xdotool key 2
+    xdotool keydown space
+    sleep 0.35
+    xdotool click 3
+    xdotool keyup space
+done
+
+xdotool type --delay 50 "/tp Sirikon 1930 151 10012 0 -90"
+sleep 1
+xdotool key Return
+sleep 1
+
+for i in {1..5}
 do
     xdotool key 1
     xdotool keydown space
